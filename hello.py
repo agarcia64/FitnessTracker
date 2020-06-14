757a1ee2713d6a3cd0476255dc203cdeb90d1a90
Alfredo <alfredo@localhost.localdomain>
2020-06-14 16:25:58 -0400

    Added templates and mongodb

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import render_template
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def home_page():
+    return render_template("index.html", name="alfredo")
 
